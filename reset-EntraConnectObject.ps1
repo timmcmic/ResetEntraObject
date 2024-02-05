63f7a72dc8c007f9d843d3bed59e25882443d19a
Timothy McMichael <timmcmic@microsoft.com>
2024-02-05 16:24:26 -0500

    Updating

---
+++ reset-EntraConnectObject.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID f9cfe327-869f-410e-90e3-7286c94c31fd
+
+.AUTHOR timmcmic@microsoft.com
+
+.COMPANYNAME Microsoft CSS
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ This script will allow an administrator to purge objects from the connetor space and perform full object sync. 
+
+#> 
+Param()
+
+write-host "Test"
